d30d8de7ec7b4b779d6b1bfb3186963a77accab9
marcoapg <marcoapg1398@gmail.com>
2022-09-12 09:36:52 -0500

    test

--- project/views.py
+++ project/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 def saludo(request):
 
-    return HttpResponse("Hola mundo")
+    return HttpResponse("Hola mundo cruel .l.")
 
 def despedida(request):
     return HttpResponse("Chau")
