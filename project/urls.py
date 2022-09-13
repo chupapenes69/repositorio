af91f696c2cf4f97c2cb86a98e09cbee5c20c73d
marcoapg <marcoapg1398@gmail.com>
2022-09-12 23:37:57 -0500

    funciones basicas mas plantillas

--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from project.views import saludo,despedida
+from project.views import dameFecha,saludo,despedida,calculaEdad
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
     path('despedida/', despedida),
+    path('damefecha/', dameFecha),
+    path('edades/<int:edad>/<int:agno>/',calculaEdad)
 ]
--- project/views.py
+++ project/views.py
@@ -1,8 +1,42 @@
 from django.http import HttpResponse
+import datetime
+
+from django.template import Template, Context
 
 def saludo(request):
 
-    return HttpResponse("Hola mundo cruel .l.")
+    doc_externo=open("plantillas/miplantilla.html")
+    plt=Template(doc_externo.read())
+
+    doc_externo.close()
+
+    ctx=Context()
+
+    documento=plt.render(ctx)
+
+    return HttpResponse(documento)
 
 def despedida(request):
     return HttpResponse("Chau")
+
+def dameFecha(request):
+
+    fecha_actual=datetime.datetime.now()
+
+    documento="""
+    <body>
+    <h2>
+    Fecha y hora actuales %s
+    </h2>
+    </body>
+    </html>""" % fecha_actual
+
+    return HttpResponse(documento)
+
+def calculaEdad(request,edad,agno):
+
+    periodo=agno-2022
+    edadFutura=edad+periodo
+    documento="<body><h2>En el año %s tendrás %s años</h2></body></html>" %(agno,edadFutura)
+
+    return HttpResponse(documento)
