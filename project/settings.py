44e01616ee44a1db66f0e2cb6b28ea452916c277
marcoapg <marcoapg1398@gmail.com>
2022-09-17 01:39:33 -0500

    changes apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appArbitro',
+    'appCompeticion',
+    'appContrato',
+    'appEquipo',
+    'appPartido',
 ]
 
 MIDDLEWARE = [
