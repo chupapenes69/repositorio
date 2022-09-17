47164aca24b5a5b7e375fedee584495bd449ef62
marcoapg <marcoapg1398@gmail.com>
2022-09-17 00:47:52 -0500

    Delete project directory

--- project/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-import datetime
-
-from django.template import Template, Context
-from django.template.loader import get_template
-from django.shortcuts import render
